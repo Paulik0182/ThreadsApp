5d4d9eb03733c02e74990d1039d8362249d4c08b
Pavel <p21011982n@yandex.ru>
2022-06-10 15:16:03 +0300

    Работа run(), создание очереди

--- app/src/main/java/com/android/threadsapp/MyWorkerThread.java
+++ app/src/main/java/com/android/threadsapp/MyWorkerThread.java
@@ -1,9 +1,13 @@
 package com.android.threadsapp;
 
+import java.util.LinkedList;
+
 /**
  * MyWorkerThread extends Thread - расширяем обычный Thread
  */
 public class MyWorkerThread extends Thread {
+
+    private final LinkedList<Runnable> queue = new LinkedList<Runnable>();//некая очередь
 
     /**
      * После выполнения работы в методе run(), данный поток умирает и его повторно вызвать нельзя.
@@ -14,16 +18,15 @@ public class MyWorkerThread extends Thread {
     @Override
     public void run() {
         while (!Thread.interrupted()) {// бесконечно будет выполнятся работа
-            doWork();
+            if (!queue.isEmpty()) {//если очередь не пуста
+                queue.get(0).run();//берем первый элемент и запускаем его
+                queue.remove(0);// далее удаляем элемент
+
+            }
         }
     }
 
-    //какаята работа
-    private void doWork() {
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    private void post(Runnable runnable) {
+        queue.add(runnable);//кладем в очередь дополнительную задачу
     }
 }
