5582cfc57580bfdb18c4ff8773ae1b4d17f61e46
Pavel <p21011982n@yandex.ru>
2022-06-10 11:37:29 +0300

    Запускаем поток. Ставим в очередь работу (Handler)

--- app/src/main/java/com/android/threadsapp/MainActivity.java
+++ app/src/main/java/com/android/threadsapp/MainActivity.java
@@ -1,22 +1,11 @@
 package com.android.threadsapp;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import com.android.threadsapp.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +18,40 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        startMyWorkerThread();//запустили паралельный поток
+
+//        returnToMainThread();
+        Handler handler = new Handler();//данный код не корректно читать последовательно.
+        handler.post(() -> {//после этой строчки сразу попадаем на метод initView().
+            //работа будет выполнена тогда когда до него дойдет очередь.
+            //todo (выполнения какойто работы.)
+        });
+
+        initView();
+
+    }
+
+    /**
+     * onCreate - главный поток
+     * В методе onCreate -> С начало стартует метод (запускаем, создаем поток) startMyWorkerThread()
+     * потом, сразу, не дожидаясь выполнения работ метода startMyWorkerThread() переходим к
+     * выполнению метода initView(). Предыдущий метод работает паралельно.
+     */
+
+    private void startMyWorkerThread() {
+        Thread thread = new MyWorkerThread();
+        thread.start();
+    }
+
+    private void initView() {
+        //todo (инициализация View. это если нет binding)
+    }
+
+    private void returnToMainThread() {
+        Handler handler = new Handler();
+        handler.post(() -> {
+            //todo (выполнения какойто работы)
+        });
     }
 
 }
