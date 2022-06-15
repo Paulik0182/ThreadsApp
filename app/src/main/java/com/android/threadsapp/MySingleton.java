04854d00c621305eb9120436faf2bb75d5e3a919
Pavel <p21011982n@yandex.ru>
2022-06-15 16:18:27 +0300

    Singleton -> пример создания своего Singleton

--- app/src/main/java/com/android/threadsapp/MySingleton.java
+++ app/src/main/java/com/android/threadsapp/MySingleton.java
@@ -7,7 +7,7 @@ public class MySingleton {
 
     //НО благодаря методу createNewInstance() появляется следующая возможность.
     //В указанном методе ставим условие для создания только одного экземпляра.
-    private static final MySingleton INSTANCE = null;
+    private static MySingleton INSTANCE = null;
 
     //необходимо запретить создавать более одного Singleton, для этого мы сделали конструктор
     // приватный, но нам необходимо создать один объект
@@ -18,6 +18,9 @@ public class MySingleton {
     //делаем возвращаемый метод чтобы можно было создать объект
     //посути это тоже самое что вызвать конструктор
     public static MySingleton createNewInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new MySingleton();
+        }
         return new MySingleton();
     }
 }
